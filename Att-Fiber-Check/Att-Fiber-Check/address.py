43c7b6235a5dca4ccdb83d108a9a8099ff8a93cb
Budman17r <Budman17r@gmail.com>
2017-02-20 15:00:21 -0600

    Still setting up the environment

--- Att-Fiber-Check/Att-Fiber-Check/address.py
+++ Att-Fiber-Check/Att-Fiber-Check/address.py
@@ -1,4 +1,4 @@
-from pygeocoder import Geocoder
+from pygeocoder import Geocoder,GeocoderError
 import configparser
 settings = configparser.ConfigParser()
 settings._interpolation = configparser.ExtendedInterpolation()
@@ -7,101 +7,203 @@ api = settings.get('SectionOne', 'geocodingapi')
 import time
 import re
 import sys
-import socks
-import socket
-import csv
 import random
+import os.path
 
-###Pulling Settings
-apiquerylimit = settings.get('SectionOne', 'apiquerylimit')
-useproxy = settings.get('SectionOne', 'useproxy')
-useapi = settings.get('SectionOne', 'useapi')
-
-
-
-##### Setting Variables in the Script. 
-if 'true' in useapi:
-	geocoder = Geocoder(api_key=api) 
-else: 
-	geocoder = Geocoder()
-if 'true' in useproxy:
-	print('boo')
-#else:
-#	socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="72.205.210.107", port=23952)
-#	socket.socket = socks.socksocket
+#File Names
+geocoder = Geocoder()
+workdone = "workdone"
+test = "test"
+filename = "addresses"
+testtobedone = "testtobedone"
+toberemoved = []
 
 
-######## Configuring Proxy Config
-#proxfile = open('proxies.cxv','rt', encoding='utf-8-sig')
-#reader = csv.DictReader(proxfile)
+###Old write File
+#f = open(filename,'w')
+#f.close()
+#f = open(filename,'w')
+#f.close()
+latlong = []
+completed = []
 
-#proxylist = []
-#for row in reader:
-#    proxylist.append(row)
-#def getproxy():
-#
-#countofproxy = len(proxylist)
-#randproxy = random.randint(1,countofproxy)
-#currproxy = proxylist[randproxy)
+geocoder = Geocoder()
 
-
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    filename = "addresses"
-
-f = open(filename,'w')
-f.close()
-
-
-address_1 = input('Enter first address: ')
-radius = float(input('Enter radius to check (meters, 100 min): '))
-
-try:
-    address_1_coords = geocoder.geocode(address_1).coordinates
-except:
-    print("API is Overlimit")
-#    socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="189.103.137.237", port=46423)
-#    socket.socket = socks.socksocket
-#    address_1_coords = geocoder.geocode(address_1).coordinates
-
-
-center_lat=address_1_coords[0]
-center_lon=address_1_coords[1]
-
-start_lat=center_lat+(radius*0.001/111.3)
-start_lon=center_lon-(radius*0.001/111.3)
-end_lat=center_lat-(radius*0.001/111.3)
-end_lon=center_lon+(radius*0.001/111.3)
 
 sleep_cycle=0
 
-curr_lat = start_lat
-curr_lon = start_lon
+#if len(sys.argv) > 1:
+#    filename = sys.argv[1]
+#else:
+#    filename = "addresses"
 
-while curr_lon <= end_lon:
+
+
+
+
+#iaddress = ""
+
+#iradius = float(1600)
+def remove_duplicates(list):
+    newlist = []
+    for i in list:
+        if i not in newlist:
+            newlist.append(i)
+    return newlist
+
+def generate_coords(faddress, sradius):
+
     try:
-        results = geocoder.reverse_geocode(curr_lat, curr_lon)
+        address_1_coords = geocoder.geocode(faddress).coordinates
+    except GeocoderError as e:
+        print('Geocoder: '+str(e))
+        input("Press Enter to exit...")
+        sys.exit()
+
+    center_lat=address_1_coords[0]
+    center_lon=address_1_coords[1]
+    #center_lat=38.559086
+    #center_lon=-90.40435699999999
+    
+    start_lat=center_lat+(sradius*0.001/111.3)
+    start_lon=center_lon-(sradius*0.001/111.3)
+    end_lat=center_lat-(sradius*0.001/111.3)
+    end_lon=center_lon+(sradius*0.001/111.3)
+    
+    sleep_cycle=0
+    
+    curr_lat = start_lat
+    curr_lon = start_lon
+
+    thelist = []
+    while curr_lon <= end_lon:
+        try:
+            #results = geocoder.reverse_geocode(curr_lat, curr_lon)
+            thelist.append([curr_lat, curr_lon])
+            #curwork = "%s, %s"%(curr_lat, curr_lon)
+            #f = open(workdone,'a')
+            #f.write(curwork+"\n")
+    
+            if curr_lat > end_lat:
+                curr_lat = curr_lat - .001
+            else:
+                curr_lat = start_lat
+                curr_lon = curr_lon + .001
+        except GeocoderError as e:
+            print('Geocoder: '+str(e))
+            input("Press Enter to exit...")
+            sys.exit()
+    thelist = remove_duplicates(thelist)
+    return thelist
+
+
+def test_address(curr_lat, curr_lon):
+    try:
+        results = geocoder.reverse_geocode(float(curr_lat), float(curr_lon))
         if re.search("^(\d+)$",str(results[0]).split()[0]) is not None:
             l = str(results[0]).split(',')
             addr = ("%s,%s,%s"%(l[0],l[1].strip(),l[2].split()[1]))
+            accurate_coords = geocoder.geocode(addr).coordinates
+            addr = "%s,%s,%s,ROOFTOP"%(addr,accurate_coords[0], accurate_coords[1])
             print (addr)
-            f = open(filename,'r')
-            if addr not in f.read():
-                accurate_coords = geocoder.geocode(addr).coordinates
-                addr = "%s,%s,%s,ROOFTOP"%(addr,accurate_coords[0], accurate_coords[1])
-                f = open(filename,'a')
-                f.write(addr+"\n")
-            f.close()
-        if curr_lat > end_lat:
-            curr_lat = curr_lat - .001
-        else:
-            curr_lat = start_lat
-            curr_lon = curr_lon + .001
-        sleep_cycle = sleep_cycle + 1
-        if sleep_cycle == 4:
-            time.sleep(1)
-            sleep_cycle = 0
-    except:
-        continue
+            return addr
+    except GeocoderError:
+        print("error: ", GeocoderError.G_GEO_OVER_QUERY_LIMIT)
+        input("Press Enter to exit...")
+        sys.exit()
+    
 
+
+    
+###IF we don't do this we get duplicates in the list and that just causes more API Hits. 
+def remove_duplicates(list):
+    newlist = []
+    for i in list:
+        if i not in newlist:
+            newlist.append(i)
+    return newlist
+
+###Add a Resume Function here
+shouldcontinue = input('Should we continue an existing job?(y or yes)')
+
+if 'y' in shouldcontinue:
+    if os.path.exists(workdone) == True:
+        print("File Exists")
+        curworking = open('workdone','r')
+        for g in curworking.readlines():
+            g = g.strip('\n')
+            g = g.strip()
+            
+            latlong.append(g.split(','))
+
+    else:
+        print("file Doesn't Exist")
+        #file2 = open(workdone,'w')
+        #file2.close()
+        f = open(filename,'w')
+        f.close()
+else:
+    iaddress = input('Enter first address: ')
+    iradius = float(input('Enter radius to check (meters, 100 min): '))
+    latlong = generate_coords(iaddress, iradius) 
+
+apiquerylimit = int(input('Api limit (Recommend 1000)'))
+#print(latlong)
+
+
+
+
+
+
+
+
+
+
+
+
+
+####MAIN BODY
+
+#adding a catch for less matches then query limit
+if len(latlong) < apiquerylimit:
+    apiquerylimit = len(latlong)
+
+count = 0
+while count < apiquerylimit:
+    curraddr = test_address(latlong[count][0],latlong[count][1])
+    toberemoved.append(latlong[count])
+    if curraddr is not None:
+        completed.append(curraddr)
+    sleep_cycle = sleep_cycle + 1
+    if sleep_cycle == 4:
+        time.sleep(1)
+        sleep_cycle = 0
+    count = count + 1
+    print(count)
+
+####Removes Items We Already Did
+for t in toberemoved:
+    latlong.remove(t)
+
+#for t in latlong:
+#    curwork = "%s, %s"%(t[0], t[1])
+#    f = open(testtobedone,'a')
+#    f.write(curwork+"\n")
+
+#### writes the remaining items to file (to be worked on later
+
+remakefile = open('workdone','w')
+remakefile.close()
+for t in latlong:
+    curwork = "%s, %s"%(t[0], t[1])
+    f = open(workdone,'a')
+    f.write(curwork+"\n")
+
+completed = remove_duplicates(completed)
+
+#### writes the new completed addresses.
+for t in completed:
+    addressoutput = "%s"%(t)
+    #f = open(test,'a')
+    f = open(filename,'a')
+    f.write(addressoutput+"\n")
