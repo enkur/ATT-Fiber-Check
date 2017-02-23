4d78c3b592279a3e55118804f58167526616881f
Dave Huang <khym@azeotrope.org>
2017-02-23 02:28:56 -0600

    Query the availability check webservice directly

--- speedcheck.py
+++ speedcheck.py
@@ -1,11 +1,9 @@
 from threading import Thread
-from splinter import Browser
 from datetime import datetime
 
-import re
-import sys
-import time
+import json
 import queue
+import urllib.request
 
 filename = 'results_'+datetime.now().strftime("%Y-%m-%d")
  
@@ -19,47 +17,49 @@ e.close()
 state = input('Enter your State (2 letter): ')
 
 def test(street, city, zip, emm_stuff):
-        try_again = True
-        while (try_again):
-                try:
-                    f = open(filename,'a')
-                    browser = Browser('phantomjs')
-                    url = "https://www.att.com/shop/unified/availability.html"
-                    browser.visit(url)
-                    browser.find_by_id('streetaddress').fill(street)
-                    browser.find_by_id('zipcode').fill(zip)
-                    browser.find_by_xpath('//*[@id="content"]/div/div[2]/div[1]/div/div/div/form/div[2]/input').first.click()
-                    if browser.is_text_present('I want to select my apartment', wait_time=10):
-                        browser.choose('selectAddressOption','2')
-                        browser.choose('subAddressList','0')
-                        browser.find_by_xpath('//*[@id="content"]/div/div[3]/div/div[3]/p/a').first.click() 
-                    if browser.is_text_present('your home qualifies for AT&T Fiber', wait_time=10):
-                        speed = '1000'
-                    elif browser.is_text_present('You can get AT&T Fiber at your home', wait_time=10):
-                        speed = '1000'
-                    elif browser.is_text_present('Mbps', wait_time=10):
-                        if browser.is_text_present('Select the services you’re interested in'):
-                                element = browser.find_by_xpath('//*[@id="offerTilesDiv"]/div[1]/div[1]/div/div[5]/div[2]/div[2]/p[1]/span[2]')
-                                speed = re.search("(\d+)",element.text).group(0)
-                        else:
-                                element = browser.find_by_xpath('//*[@id="content"]/div/div[1]/div[5]/div[1]/div/div[2]/div[2]/div/span/div/div[2]/p[1]/span')
-                                speed = re.search("(\d+)",element.text).group(0)
-                    else:
-                        speed = '0'
-                    if speed == '0':
-                            print ('bad address')
-                    else:
-                            print (street+', '+city+', '+state+', '+zip+', '+emm_stuff[0]+', '+emm_stuff[1]+', '+emm_stuff[2]+', '+speed)
-                            f.write(street+', '+city+', '+state+', '+zip+', '+emm_stuff[0]+', '+emm_stuff[1]+', '+emm_stuff[2]+', '+speed+'\n')
-                    browser.quit()
-                    try_again=False
-                    f.close()
-                except:
-                    try_again=False
-                    e = open('bad_addresses','a')
-                    e.write(street+', '+city+', '+state+' '+emm_stuff[0]+', '+emm_stuff[1]+', '+emm_stuff[2]+', '+zip+'\n')
-                    e.close()
-                    browser.quit()
+    jsoncontent = {'Content-Type': 'application/json'}
+
+    try_again = True
+    while (try_again):
+        try:
+            f = open(filename,'a')
+            url = "https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service/CheckAvailabilityRESTService/invokeCheckAvailability"
+            postdata = json.dumps({
+                'userInputZip': zip,
+                'userInputAddressLine1': street,
+                'mode': 'fullAddress'}).encode(encoding='utf-8')
+            req = urllib.request.Request(url=url, data=postdata, headers=jsoncontent, method='POST')
+            resp = urllib.request.urlopen(req)
+            if resp.getcode() == 200:
+                speed = None
+                respobj = json.loads(resp.read().decode('utf-8'))
+                status = respobj['wbfcResponse']['statusName']
+                if status == 'Success':
+                    speed = respobj['CkavDataBean']['maxHsiaSpeedAvailable']
+                    if speed is None:
+                        print('No service available at %s' % street)
+                    elif speed.endswith(' Mbps'):
+                        speed = speed[:-5]
+                else:
+                    print('Availability check returned status %s' % status)
+
+                if speed is not None:
+                    line = '%s, %s, %s, %s, %s, %s, %s, %s' % \
+                        (street, city, state, zip, emm_stuff[0], emm_stuff[1], emm_stuff[2], speed)
+                    print(line)
+                    f.write(line + '\n')
+            else:
+                print('Availability check returned HTTP status %d' % resp.getcode())
+
+            try_again=False
+            f.close()
+
+        except Exception as x:
+            print(x)
+            try_again=False
+            e = open('bad_addresses','a')
+            e.write(street+', '+city+', '+state+' '+emm_stuff[0]+', '+emm_stuff[1]+', '+emm_stuff[2]+', '+zip+'\n')
+            e.close()
                
 def run_test(i):
     i = i.strip()
